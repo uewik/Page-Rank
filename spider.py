9de5a43e34367137397750bd25e2940e4e265195
Hao Zhang <howezhang88@gmail.com>
2017-05-09 19:37:51 -0700

    Update spider.py

--- spider.py
+++ spider.py
@@ -4,14 +4,21 @@ from urlparse import urljoin
 from urlparse import urlparse
 from BeautifulSoup import *
 
+# Deal with SSL certificate anomalies Python > 2.7
+# scontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+
 conn = sqlite3.connect('spider.sqlite')
 cur = conn.cursor()
+
 cur.execute(''' CREATE TABLE IF NOT EXISTS Pages
     (id INTEGER PRIMARY KEY, url TEXT UNIQUE, html TEXT,
      error INTEGER, old_rank REAL, new_rank REAL)''')
+
 cur.execute(''' CREATE TABLE IF NOT EXISTS Links
     (from_id INTEGER, to_id INTEGER)''')
+
 cur.execute(''' CREATE TABLE IF NOT EXISTS Webs (url TEXT UNIQUE)''')
+
 #check to see if we are already in progress..
 cur.execute('SELECT id, url FROM Pages WHERE html IS NULL AND error IS NULL ORDER BY RANDOM() LIMIT 1')
 row = cur.fetchone()
@@ -26,14 +33,20 @@ else:
     if starturl.endswith('.htm') or starturl.endswith('.html'):
         pos = starturl.rfind('/');
         web = starturl[:pos]
+        
     if len(web) > 1:
         cur.execute('INSERT OR IGNORE INTO Webs (url) VALUES (?)', (web, ))
         cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES (?, NULL, 1.0)', (web, ))
+        conn.commit()
+        
+# Get the current webs
 cur.execute('SELECT url FROM Webs')
 webs = []
 for row in cur:
     webs.append(str(row[0]))
+    
 print webs
+
 many = 0
 while True:
     if many < 1:
@@ -41,6 +54,7 @@ while True:
         if len(sval) < 1: break
         many = int(sval)
     many = many - 1
+    
     cur.execute('SELECT id, url FROM Pages WHERE html IS NULL AND error IS NULL ORDER BY RANDOM() LIMIT 1')
     try:
         row = cur.fetchone()
@@ -50,21 +64,33 @@ while True:
         print "No unretrieved HTML pages found"
         many = 0;
         break
+        
     print fromid, url
-    cur.execute('DELETE FROM Links WHERE from_id = ?', (fromid, ))  # Just make sure
+    
+    # If we are retrieving this page, there should be no links from it
+    cur.execute('DELETE FROM Links WHERE from_id = ?', (fromid, ))
     try:
+        # Deal with SSL certificate anomalies Python > 2.7
+        # scontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+        # document = urllib.urlopen(url, context=scontext)
+
+        # Normal Unless you encounter certificate problems
         document = urllib.urlopen(url)
+        
         html = document.read()
         if document.getcode() != 200:
             print "Error on Page: ",document.getcode()
             cur.execute('UPDATE Pages SET error = ? WHERE url = ?', (document.getcode(), url))
-            cur.commit()    # added
+            cur.commit()
+            
         if 'text/html' != document.info().gettype():
             print "Ignore non text/html page"
             cur.execute('UPDATE Pages SET error = -1 WHERE url = ?', (url,))
             cur.commit()
             continue
+            
         print "("+ str(len(html)) +")"
+        
         soup = BeautifulSoup(html)
     except KeyboardInterrupt:
         print ""
@@ -75,8 +101,10 @@ while True:
         cur.execute('UPDATE Pages SET error = -1 WHERE url = ?', (url ,))
         conn.commit()
         continue
+        
     # cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES ( ?, NULL, 1.0 )', (url,))
     cur.execute('UPDATE Pages SET html = ? WHERE url = ?', (buffer(html), url))
+    conn.commit()
 
     # Retrieve all of the anchor tags
     tags = soup('a')
@@ -93,6 +121,7 @@ while True:
         if ( href.endswith('.png') or href.endswith('.jpg') or href.endswith('.gif') ) : continue
         if ( href.endswith('/') ) : href = href[:-1]
         if ( len(href) < 1 ) : continue
+            
         # Check if the URL is in any of the webs
         found = False
         for web in webs:
@@ -100,9 +129,11 @@ while True:
                 found = True
                 break
         if not found: continue
+            
         cur.execute('INSERT OR IGNORE INTO Pages(url, html, new_rank) VALUES (?, NULL, 1.0)', (href, ))
         count = count + 1
         conn.commit()
+        
         cur.execute('SELECT id FROM Pages WHERE url = ? LIMIT 1', (href, ))
         try:
             row = cur.fetchone()
@@ -110,8 +141,11 @@ while True:
         except:
             print "Could not retrive id"
             continue
+            
         cur.execute('INSERT OR IGNORE INTO Links (from_id, to_id) VALUES (?, ?)', (fromid, toid))
+        
     print count
+    
 cur.close()
 
 
